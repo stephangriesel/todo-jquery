345013984590449b3d0bbfd98349e95f2bae48ad
Stephan Griesel <sgriesel@gmail.com>
2019-02-17 16:03:47 +0100

    update edit, save button & new functions

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     // Event handlers
     $('#addItem').on('click', addItem);
     $('#todos').on('change', '.completeItem', completeItem);
-    $('#todos').on('click', '.deleteItem', deleteItem);
+    $('#todos').on('click', '.todoText', startEditing);
+    $('#todos').on('click', '.saveItem', stopEditing);
     $('#newTodo').on('keypress', function(event) {
         if(event.which === 13){
             addItem();
@@ -10,12 +11,36 @@ $(document).ready(function() {
         }
     });
 
+    function startEditing(event) {
+
+        $(this).parent().find('.todoText');
+
+        var currentText = $(this).parent().find('.todoText').text();
+        $(this).parent().find(".editText").val(currentText);
+        $(this).parent().find(".editText").show();
+        $(this).parent().find(".saveItem").show();
+        $(this).parent().find(".todoText").hide();
+    }
+
+    function stopEditing(event) {
+        // hide the edit text box
+        // take value from edit text box and place it in our span
+        // show our span
+        $(this).hide();
+        var newValue = $(this).parent().find(".editText").val();
+        $(this).parent().find(".editText").hide();
+        $(this).parent().find('.todoText').text(newValue);
+        $(this).parent().find(".todoText").show();
+
+
+    }
+
 
     function addItem (){
         var newTodoText = $('#newTodo').val();
         $();
         $('#todos').append
-            ('<li><input class="completeItem" type="checkbox">' + newTodoText + '<span class="glyphicon glyphicon-trash deleteItem"></span></li>' );
+            ('<li><input class="completeItem" type="checkbox"> <span class="todoText">' + newTodoText + '</span><input type="text" class="editText"><button class = "btn btn-success saveItem">save</button><span class="glyphicon glyphicon-trash deleteItem"></span></li>' );
         $('#newTodo').val("");
 
     }
